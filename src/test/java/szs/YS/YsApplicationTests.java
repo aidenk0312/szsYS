dcef380bbfb797659d87041b0d0d75f691be8bfa
yongsu kim <yongsukim@yongsuui-MacBookPro.local>
2024-01-17 22:18:24 +0900

    변경사항: 초기세팅 AS-IS
    TO-BE
    
    테스트 (대상아님)
     테스트 코드
     API 테스트

---
+++ src/test/java/szs/YS/YsApplicationTests.java
@@ -0,0 +1,13 @@
+package szs.YS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
